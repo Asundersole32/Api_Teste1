c379df85b67c1b34162bd8d49bf9d513595ab0af
Erik Felipe(PC casa) <erikviana32@gmail.com>
2023-07-16 03:05:20 -0400

    Começo das Rotas de Segurança

--- API/Ui/routes.py
+++ API/Ui/routes.py
@@ -1,3 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
+from Models import token_model
+
+from Application import security
+from datetime import timedelta
+
+from typing import Annotated
+
+
+app = FastAPI()
+
+@app.post("/token", response_model=token_model.Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = security.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=security.accessTokenExpireMinutes)
+    access_token = security.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
